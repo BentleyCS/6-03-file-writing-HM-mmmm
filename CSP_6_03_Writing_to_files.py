80fd6d27eaf507ae9791d60266db28d658ed2c26
HM-mmmm <hmackenzie26@bentleyschool.org>
2026-03-03 11:44:23 -0800

    Add files via upload

--- CSP_6_03_Writing_to_files.py
+++ CSP_6_03_Writing_to_files.py
@@ -5,22 +5,40 @@ import random
 
 def writeFile(inputList, fileName):
     #Creates a file of the given name and adds each value from the list to said file with each line being an index from the list.
-    pass
+    with open(fileName, "w") as file:
+        for n in inputList:
+            file.write(f"{n}\n")
+        return fileName
 
 
-def sortNames(fileName, targetFile):
+def sortNames(sourceName, targetName):
     #Modify the below function such that it takes in source file and a target file.
     #Sort all of the values from the source file and write them to the target file
     #I recommend using .sort() for this. You do not need to write the sorting algorithm yourself.
-    pass
+    randomList = []
+    with open(sourceName, "r") as sourceFile:
+        with open(targetName, "w") as targetFile:
+            for line in sourceFile:
+                randomList.append(line)
+            randomList.sort()
+            for n in randomList:
+                targetFile.write(n)
+# sortNames("names.txt","namesNew.txt")
 
-sortNames("names.txt","namesNew.txt")
-
-
+def get_average(numbers):
+    return sum(numbers) / len(numbers) if numbers else 0
 
 def highScore( newScore: int):
     #Modify the function such that it adds a new score to the file scores.txt
     #Then return the average score from all of the scores in scores.txt
-    pass
-highScore(random.randint(1,100))
+    scoreList = []
+    with open("scores.txt", "a") as file:
+        file.write(f"{newScore}\n")
+    with open("scores.txt", "r") as file:
+        for line in file:
+            scoreList.append(int(line))
+    avg = get_average(scoreList)
+    return(avg)
+int(highScore(30))
+# highScore(random.randint(1,100))
 
---
+++ test_CSP_6_03_Writing_to_files.py
@@ -0,0 +1,24 @@
+import CSP_6_03_Writing_to_files as HW
+
+def test_write_file():
+    HW.writeFile([1,2,3], "newFile1")
+    with open("newFile1", "r") as file:
+        assert file.readline(100) == "1\n"
+        assert file.readline(100) == "2\n"
+        assert file.readline(100) == "3\n"
+
+
+def test_sort_names():
+    HW.sortNames("/Users/hayden/PycharmProjects/PythonProject/AP_Principles_2026/January - Feb/6-03-file-writing-HM-mmmm-main/inputNames", "targetName")
+    with open("targetName", "r") as file:
+        assert file.readline(100) == "Hayden\n"
+        assert file.readline(100) == "Jade\n"
+        assert file.readline(100) == "Jessica\n"
+        assert file.readline(100) == "Lindsey\n"
+
+
+def test_high_score():
+    with open("scores.txt", "w") as file:
+        file.write("10\n20\n40\n50\n")
+    assert HW.highScore(30) == 30
+    assert HW.highScore(60) == 35
